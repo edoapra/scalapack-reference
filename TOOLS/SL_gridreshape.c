e43210c04982998bbe8acdb6295d28cb43b5b70b
edoapra <edoardo.apra@gmail.com>
2025-02-19 14:26:15 -0800

    removing conflicting declarations to make code compile with gcc 15 https://gcc.gnu.org/gcc-15/porting_to.html#c23

--- TOOLS/SL_gridreshape.c
+++ TOOLS/SL_gridreshape.c
@@ -8,7 +8,6 @@
 #include "../PBLAS/SRC/PBblacs.h"
 Int SL_Cgridreshape(Int ctxt, Int pstart, Int row_major_in, Int row_major_out, Int P, Int Q)
 {
-   Int Cblacs_pnum();
    Int nctxt, P0, Q0, Np, i, *g;
 
    Cblacs_gridinfo(ctxt, &P0, &Q0, &i, &Np);
